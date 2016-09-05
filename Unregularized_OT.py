f7b7c14852c056365918c06f0cfd5e321081cafd
BorisMuzellec <boris.muzellec@polytechnique.edu>
2016-09-05 17:48:56 +0200

    Implemented unregularized OT using a linear solver

---
+++ Unregularized_OT.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  5 16:20:04 2016
+
+@author: boris
+"""
+from scipy.optimize import linprog
+import numpy as np
+
+
+#Function using a linear solver to solve unregularized OT
+def Unregularized_OT(M,r,c):
+    
+    n = M.shape[0]
+    m = M.shape[1]
+    
+    #Create the constraint matrix    
+    Ac = np.zeros((m,n*m))
+    for i in range(m):
+        for j in range(n):
+            Ac[i,i+j*m] = 1
+                
+    
+    
+    Ar = np.zeros((n,n*m))
+    for i in range(n):
+        for j in range(m):
+            Ar[i,m*i+j] = 1
+ 
+            
+    
+    res = linprog(M.flatten(),A_eq = np.vstack((Ar,Ac)),b_eq=np.hstack((r,c)))
+    
+    return res.x.reshape((n,m))
+    
